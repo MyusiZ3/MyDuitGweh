8975239664d5cadacb777a452b31bc15cb898d8b
imyusi_ <zayyan3002@gmail.com>
2026-09-09 06:34:38 +0700

    fix(onboarding): remove unused math import

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:math' as math;
 
 class OnboardingScreen extends StatefulWidget {
   final VoidCallback? onComplete;
